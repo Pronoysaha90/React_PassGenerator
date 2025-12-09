84b214cf3218fbf4979ff837ebcd9af8543a35bd
pronoy <pronoysaha623@gmail.com>
2025-12-09 23:26:14 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text text-transparent bg-clip-text bg-linear-to-r from-cyan-500 to-sky-500 font-semibold text-2xl'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
